c26058d171eec4bddefcd3e9e37ae426cc02b274
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-11-11 19:40:29 -0800

    用户服务程序入口java文件

---
+++ src/main/java/io/cde/account/Application.java
@@ -0,0 +1,17 @@
+package io.cde.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 程序入口
+ * @author lcl
+ * @createDate 2016年11月2日上午11:45:43
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
