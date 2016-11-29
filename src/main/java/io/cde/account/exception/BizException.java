67a91e592e9f6e16fcbc49e34929b3c87c17812a
llichangingithub <lichanglin@zyeeda.com>
2016-11-29 14:31:20 +0800

    修改BizException，增加对ErrorCode的处理

--- src/main/java/io/cde/account/exception/BizException.java
+++ src/main/java/io/cde/account/exception/BizException.java
@@ -1,5 +1,7 @@
 package io.cde.account.exception;
 
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 /**
  * @author lcl
  * @createDate 2016年11月29日下午1:59:24
@@ -9,11 +11,27 @@ public class BizException extends Exception {
 
 	private static final long serialVersionUID = -5937698721553801873L;
 	
-	public BizException(String message) {
-		super(message);
-	}
+	private int code;
+	private String message;
 	
-	public BizException(String message, Throwable cause) {
-		super(message, cause);
+	public BizException(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 }
