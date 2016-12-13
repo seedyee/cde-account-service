c278d40eff92aaeadb5346c4ab2c2a80834cfaa7
llichangingithub <lichanglin@zyeeda.com>
2016-12-13 15:53:28 +0800

    添加fillter

--- src/main/java/io/cde/account/Application.java
+++ src/main/java/io/cde/account/Application.java
@@ -3,6 +3,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+
+import io.cde.account.tools.MyFillter;
 
 /**
  * 程序入口.
@@ -10,11 +15,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lcl
  */
 @SpringBootApplication
+@ServletComponentScan
 public class Application {
 
 	public static void main(String[] args) {
 		Logger logger = LoggerFactory.getLogger(Application.class);
 		logger.info("service start");
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public static FilterRegistrationBean init(){
+		 return new FilterRegistrationBean(new MyFillter());
 	}
 }
---
+++ src/main/java/io/cde/account/tools/MyFillter.java
@@ -0,0 +1,51 @@
+package io.cde.account.tools;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;;
+/**
+ * @author lcl
+ *
+ */
+@WebFilter(filterName = "myfilter", urlPatterns = "/*")
+public class MyFillter implements Filter{
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletResponse response2 = (HttpServletResponse) response;
+		response2.setHeader("Access-Control-Allow-Origin", "http://192.168.1.25:1337");
+		response2.setContentType("application/json");
+		chain.doFilter(request, response2);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
