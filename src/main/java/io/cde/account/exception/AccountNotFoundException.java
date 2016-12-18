6d5b2ca44ae5905402c4cb1d2f0e5f442692c6b4
llichangingithub <lichanglin@zyeeda.com>
2016-12-19 00:18:47 +0800

    修改exception目录下代码格式

--- src/main/java/io/cde/account/exception/AccountNotFoundException.java
+++ src/main/java/io/cde/account/exception/AccountNotFoundException.java
@@ -5,53 +5,55 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * @author lcl
- *
  */
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not found the account by id")
 public class AccountNotFoundException extends RuntimeException {
 
-	/**
-	 * 序列号.
-	 */
-	private static final long serialVersionUID = -7653664292819989061L;
+    /**
+     * 序列号.
+     */
+    private static final long serialVersionUID = -7653664292819989061L;
+
     /**
      * 异常编码.
      */
-	private int code;
-	/**
-	 * 异常信息.
-	 */
-	private String message;
-	
-	/**
-	 * 无参数构造器.
-	 */
-	public AccountNotFoundException() {}
-	
-	/**
-	 * 有参数构造器.
-	 * 
-	 * @param code 异常编码
-	 * @param message 异常信息
-	 */
-	public AccountNotFoundException(int code, String message) {
-		this.code = code;
-		this.message = message;
-	}
+    private int code;
 
-	public int getCode() {
-		return code;
-	}
+    /**
+     * 异常信息.
+     */
+    private String message;
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+    /**
+     * 无参数构造器.
+     */
+    public AccountNotFoundException() {
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    /**
+     * 有参数构造器.
+     *
+     * @param code 异常编码
+     * @param message 异常信息
+     */
+    public AccountNotFoundException(final int code, final String message) {
+        this.code = code;
+        this.message = message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(final int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
 }
--- src/main/java/io/cde/account/exception/BizException.java
+++ src/main/java/io/cde/account/exception/BizException.java
@@ -2,41 +2,49 @@ package io.cde.account.exception;
 
 /**
  * @author lcl
- * 业务异常
+ *         业务异常
  */
 public class BizException extends Exception {
-    
-	/**
-	 * 序列号.
-	 */
-	private static final long serialVersionUID = -5937698721553801873L;
-	/**
-	 * 异常编码. 
-	 */
-	private int code;
-	/**
-	 * 异常信息.
-	 */
-	private String message;
-	
-	public BizException(int code, String message) {
-		this.code = code;
-		this.message = message;
-	}
 
-	public int getCode() {
-		return code;
-	}
+    /**
+     * 序列号.
+     */
+    private static final long serialVersionUID = -5937698721553801873L;
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+    /**
+     * 异常编码.
+     */
+    private int code;
 
-	public String getMessage() {
-		return message;
-	}
+    /**
+     * 异常信息.
+     */
+    private String message;
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    /**
+     * 构造方法.
+     *
+     * @param code 错误码
+     * @param message 错误信息
+     */
+    public BizException(final int code, final String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(final int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
 }
